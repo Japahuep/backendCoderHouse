40e9eba5b121db9dac8cc4d1fd1cd01ed438b6f9
Sebastián Molina <sebastianmolinarojas@gmail.com>
2022-10-24 16:45:24 -0300

    Desafío 15 - Nginx

---
+++ challenge15Nginx/src/routers/web/info.js
@@ -0,0 +1,38 @@
+import { Router } from "express";
+import { webAuth } from "../../auth/index.js";
+import { cpus } from "os";
+
+import path from "path";
+const __dirname = path.resolve();
+const infoWebRouter = new Router();
+
+const enviromentVariablesFn = () => {
+  const inputs = [];
+  for (let i = 2; i < process.argv.length; i++) {
+    inputs.push(process.argv[i]);
+  }
+  return inputs;
+};
+const enviromentVariables = enviromentVariablesFn();
+const currentDirectory = process.cwd();
+const processId = process.pid;
+const nodeVersion = process.version;
+const proccesTitle = process.title;
+const operatingSystem = process.platform;
+const memory = process.memoryUsage().rss;
+const numCpu = cpus().length;
+
+infoWebRouter.get("/info", webAuth, (req, res) => {
+  res.render(path.join(__dirname + "/views/pages/info.ejs"), {
+    enviromentVariables,
+    operatingSystem,
+    nodeVersion,
+    memory,
+    proccesTitle,
+    processId,
+    currentDirectory,
+    numCpu,
+  });
+});
+
+export default infoWebRouter;
